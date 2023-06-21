de70df9df53aa2b56b8988fc9672908f687fd13e
Diogo Macieira <diogo.andre.macieira@gmail.com>
2023-06-21 11:08:12 +0100

    .

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,27 +3,28 @@ import Contactos from "../components/Contactos";
 import Shop from "../components/Shop";
 import Form from "../components/Form";
 import App from "../App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/contactos",
+          element: <Contactos />,
+        },
+        { path: "/shop", element: <Shop /> },
+        { path: "/form", element: <Form /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/contactos",
-        element: <Contactos />,
-      },
-      { path: "/shop", element: <Shop /> },
-      { path: "/form", element: <Form /> },
-    ],
+    basename: "/FundacaodoGil",
   }
-],
-{
-  basename:"/FundacaodoGil"
-}
 );
